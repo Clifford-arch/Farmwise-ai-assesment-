7cc4015fc2e012a3c83e0c6f99fcb661448c71e4
Clifford-arch <cliffordchristophermarx@gmail.com>
2024-01-25 22:52:24 +0530

    Step 2: Create redux store and integrate with react.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducer from "./redux/reducer";
+
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const store = createStore(reducer);
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
